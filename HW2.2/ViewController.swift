85a5bf6f4a28789fc7c5f17d5cbb4a3245dd6743
MaximShvanov <shvanov.m@yandex.ru>
2020-08-22 00:37:05 +0300

    adding functionLogic

--- HW2.2/ViewController.swift
+++ HW2.2/ViewController.swift
@@ -9,12 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var coloredView: UIView!
+    @IBOutlet var redCounterView: UILabel!
+    @IBOutlet var blueCounterView: UILabel!
+    @IBOutlet var greenCounterView: UILabel!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        forLoad()
+        
     }
-
-
+    
+    
+    @IBAction func redSliderAction() {
+        redCounterView.text = String(Int(redSlider.value * 100))
+        coloredView.backgroundColor = .init(red: redValue, green: greenValue, blue: blueValue, alpha: 1)
+    }
+    @IBAction func blueSliderAction() {
+        blueCounterView.text = String(Int(blueSlider.value * 100))
+        coloredView.backgroundColor = .init(red: redValue, green: greenValue, blue: blueValue, alpha: 1)
+    }
+    @IBAction func greenSliderAction() {
+        greenCounterView.text = String(Int(greenSlider.value * 100))
+        coloredView.backgroundColor = .init(red: redValue, green: greenValue, blue: blueValue, alpha: 1)
+    }
+    
+    private func forLoad() {
+        redSlider.value = 0
+        blueSlider.value = 0
+        greenSlider.value = 0
+        redCounterView.text = String(0)
+        blueCounterView.text = String(0)
+        greenCounterView.text = String(0)
+        coloredView.backgroundColor = .black
+    }
+    
 }
 
+extension ViewController {
+    var redValue: CGFloat { CGFloat(redSlider.value) }
+    var blueValue: CGFloat { CGFloat(blueSlider.value) }
+    var greenValue: CGFloat { CGFloat(greenSlider.value) }
+}
